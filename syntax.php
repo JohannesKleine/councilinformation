e4960dbb0e5f0db476528efb5efb195c66f4f065
Christoph Johannes Kleine <linux@cj-k.de>
2020-06-03 14:51:16 +0200

    replace old handle and render function

--- syntax.php
+++ syntax.php
@@ -121,7 +121,7 @@
         * @see render()
         * @static
         */
-        function handle($match, $state, $pos, &$handler){
+        function handle($match, $state, $pos, Doku_Handler $handler){
         /**    switch ($state) {
               case DOKU_LEXER_ENTER : 
                 break;
@@ -191,7 +191,7 @@
         * @public
         * @see handle()
         */
-        function render($mode, &$renderer, $data) {
+        function render($mode, Doku_Renderer $renderer, $data) {
             
             $table = "<div class='table sectionedit2'><table class='inline'>
                 <tbody>
